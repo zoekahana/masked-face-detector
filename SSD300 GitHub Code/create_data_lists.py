f5755b31a9b7f8a7217e284aba2f48ee12e0c423
Zoe Kahana <zkahana@princeton.edu>
2020-12-09 00:03:14 -0600

    files

---
+++ SSD300 GitHub Code/create_data_lists.py
@@ -0,0 +1,6 @@
+from utils import create_data_lists
+
+if __name__ == '__main__':
+    create_data_lists(train_path='/Users/zoekahana/Documents/__Fall2020/cos429/Final/objdet-pytorch/data/train',
+                      test_path='/Users/zoekahana/Documents/__Fall2020/cos429/Final/objdet-pytorch/data/test',
+                      output_folder='/Users/zoekahana/Documents/__Fall2020/cos429/Final/objdet-pytorch/output')
